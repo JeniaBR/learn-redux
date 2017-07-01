2c5a67b6b242bb9e12241b5774db826c0dcafaba
Jenia Brook <jeniabrook@gmail.com>
2017-07-01 21:54:42 +0300

    finished comments reducer

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -4,6 +4,7 @@ class Comments extends React.Component {
   constructor(){
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderComment = this.renderComment.bind(this);
   }
 
   renderComment(comment, i) {
@@ -12,7 +13,7 @@ class Comments extends React.Component {
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -24,6 +25,7 @@ class Comments extends React.Component {
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
 
   render(){
