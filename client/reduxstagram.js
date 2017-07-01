cd1ee7e3a475308e7ba3630b4ccb44799bc68cef
Jenia Brook <jeniabrook@gmail.com>
2017-07-01 10:50:34 +0300

    adding redux to the app store, actions. reducers

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import css from './styles/style.styl';
 
 import Main from './components/Main';
