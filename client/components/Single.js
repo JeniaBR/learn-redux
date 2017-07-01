5026d4d7d397192221b268b0cde522fea6561b00
Jenia Brook <jeniabrook@gmail.com>
2017-07-01 13:54:49 +0300

    Add Comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
+const Single = (props) =>  {
 
-class Single extends React.Component {
-  render() {
-    return (
-      <div className="single-photo">
-        I'm single photo
-      </div>
-    )
-  }
+  const {postId} = props.params;
+  const i = props.posts.findIndex((post) => post.code === postId);
+  const post = props.posts[i];
+  const postComments = props.comments[postId] || [];
+
+  return (
+    <div className="single-photo">
+      <Photo i={i} post={post} {...props} />
+      <Comments postComments={postComments} {...props}/>
+    </div>
+  )
 }
 
 export default Single;
